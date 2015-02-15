d6aebf388b601ddab3ef3b2f07d84ebba486795c
Grey <dzimel94@gmail.com>
2015-02-15 23:36:05 +0100

    Create sprite class

--- Interface/Interface/main.cpp
+++ Interface/Interface/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include "Init.h"
 #include "Renderer.h"
+#include "TextureSource.h"
 #define SDL_MAIN_HANDLED
 #undef main
 
@@ -26,12 +27,14 @@ int main(){
 
 	SDL2::Display::Window w("My window", 800, 600);
 	SDL2::Display::Renderer r(w);
-	
+	SDL2::Graphic::Sprite s("../images/images_15.bmp", r);
+
 	SDL_Event* e = new SDL_Event();
 	SDL_SetRenderDrawColor(r.get(), 0, 0, 255,255);
 	do{
 		r.clear();
 		SDL_PollEvent(e);
+		r.copy(s.texture(), &s.rectSource, &s.rectShowed);
 		r.present();
 	} while(e->type != SDL_QUIT);
 
