6e8e3a49a410fcfc9cd00ca31cd01b0962b96b4e
Grey <dzimel94@gmail.com>
2015-03-01 10:13:53 +0100

    all basic engine

--- Interface/Interface/Text.h
+++ Interface/Interface/Text.h
@@ -1,64 +1,117 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <memory>
 #include "SDL_ttf.h"
 #include "SDL_image.h"
-#include "Renderer.h"
+#include "SDL_render.h"
+#include "Event.h"
 /*Interfame to SDL2 engine*/
 namespace SDL2{
-	/*Namespace with text manipulation*/
-	namespace Text{
-		class Source{
+	/*Space with classes to Graphics share and manipulation*/
+	namespace Graphic{
+		class Text{
 		public:
-			Source(const std::string &path, const std::string &text, int size, const SDL_Color color)
-				:_color(color), _surface(nullptr), _texture(nullptr){
-				if(!(_font = TTF_OpenFont(path.c_str(), size)))
+			/*Create text texture with message
+			*@param SDL_Renderer renderer to create texture
+			*@param std::string path to file ttf
+			*@param std::string text to showed
+			*@param int size font
+			*@param SDL_Color text color*/
+			Text(SDL_Renderer* renderer, const std::string &path, const std::string &text, int size, const SDL_Color color)
+				:_color(color), _surface(nullptr), _texture(nullptr), _message(text){
+				if (!(_font = TTF_OpenFont(path.c_str(), size)))
 					std::cout << "Error with load TTF: " << TTF_GetError() << std::endl;
 				else
-					if(!(_surface = TTF_RenderText_Solid(_font, text.c_str(), _color)))
-						std::cout<< "Error with load TTF: " << TTF_GetError() << std::endl;
-				}
-			
-			Source(TTF_Font* font){}
-			~Source(){
+					if (!(_surface = TTF_RenderText_Solid(_font, text.c_str(), _color)))
+						std::cout << "Error with load TTF: " << TTF_GetError() << std::endl;
+					else
+						if (!(_texture = SDL_CreateTextureFromSurface(renderer, _surface)))
+							std::cout << "Error with create texture: " << SDL_GetError() << std::endl;
+			}
+
+			/*Create text texture with message
+			*@param TTF_Font style font
+			*@param SDL_Renderer renderer to create texture
+			*@param std::string text to showed
+			*@param int size font
+			*@param SDL_Color text color*/
+			Text(TTF_Font* font, SDL_Renderer* renderer, const std::string& text, const SDL_Color color)
+				:_color(color), _surface(nullptr), _texture(nullptr), _message(text){
+				if (!(_surface = TTF_RenderText_Solid(_font, text.c_str(), _color)))
+					std::cout << "Error with load TTF: " << TTF_GetError() << std::endl;
+				else
+					if (!(_texture = SDL_CreateTextureFromSurface(renderer, _surface)))
+						std::cout << "Error with create texture: " << SDL_GetError() << std::endl;
+			}
+
+			~Text(){
+				SDL_DestroyTexture(_texture);
+				SDL_FreeSurface(_surface);
 				TTF_CloseFont(_font);
 			}
 
+			/*SDL_Texture getter
+			*@return SDL_Texture*/
 			SDL_Texture* texture()const{
 				return _texture;
 			}
+
+			/*Set new from keyboard
+			*@param SDL_Renderer renderer to clear, copy and present on screen
+			*@param SDL2::Graphic::Sprite array to show under text
+			*@param uint32 size to array*/
+			bool input(SDL_Renderer* renderer, SDL2::Graphic::Sprite sprites[], uint32_t size){
+				bool success = false;
+				bool quit = false;
+				SDL2::Events::Event ev;
+				SDL_StartTextInput();
+				std::string last = _message;
+				while (!quit){
+					if (ev.poll()){
+						SDL_RenderClear(renderer);
+
+						if (ev.key() == SDLK_BACKSPACE && _message.length())
+							_message.pop_back();
+						else if (ev.key() == SDLK_ESCAPE){
+							_message = last;
+							quit = true;
+						}
+						else if (ev.key() == SDLK_RETURN)
+							quit = true;
+						else if (ev.type() == SDL_TEXTINPUT)
+							_message += ev.get().text.text;
+
+						refresh(renderer);
+						for (auto i = 0; i < size; ++i)
+							SDL_RenderCopy(renderer, sprites[i].source()._Get()->texture(), &sprites[i].rectSource, &sprites[i].rectShowed);
+						SDL_RenderCopy(renderer, _texture, nullptr, &rect);
+						SDL_RenderPresent(renderer);
+					}
+				}
+				SDL_StopTextInput();
+				return success;
+			}
+
+			SDL_Rect rect;
 		protected:
 			TTF_Font* _font;
 			SDL_Surface* _surface;
 			SDL_Texture* _texture;
 			SDL_Color _color;
-		};
 
-		class Output: public Source{
-		public:
-			Output(const Display::Renderer& renderer, const std::string &path, const std::string &text, int size, const SDL_Color color):
-			Source(path, text, size, color), _message(text){
-				if(!(_texture = SDL_CreateTextureFromSurface(renderer.get(), _surface)))
-					std::cout << "Error with create texture: " << SDL_GetError() << std::endl;
-			}
-			const std::string& message()const{
-				return _message;
-			}
-
-			SDL_Rect rect;
-		private:
 			std::string _message;
-			
-		};
 
-		class Input{
-		public:const std::string& message()const{
-					return _message;
-				}
-
-					   SDL_Rect rect;
 		private:
-			std::string _message;
+			bool refresh(SDL_Renderer* renderer){
+				SDL_DestroyTexture(_texture);
+				SDL_FreeSurface(_surface);
+
+				_surface = TTF_RenderText_Solid(_font, _message.c_str(), _color);
+				_texture = SDL_CreateTextureFromSurface(renderer, _surface);
+
+				return (_texture && _surface) ? true : false;
+			}
 		};
 	}
 }
